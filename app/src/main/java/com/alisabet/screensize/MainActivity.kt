b5ee4d7f9924ebe3585aa70e2be79bb42229ecfc
Ali <mailalisabet@gmail.com>
2018-03-24 19:51:08 +0430

    march 24 / 2018

--- app/src/main/java/com/alisabet/screensize/MainActivity.kt
+++ app/src/main/java/com/alisabet/screensize/MainActivity.kt
@@ -10,8 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val result = Screen.getScreenDP(this)
-        Toast.makeText(this, result, Toast.LENGTH_LONG).show()
-
     }
 }
